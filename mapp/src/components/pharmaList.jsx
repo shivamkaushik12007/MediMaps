88c7a171c3296fcd8255b38ed428ae004219593e
kaushik <shivamkaushik12007@gmail.com>
2020-01-18 19:53:52 +0530

    location constrain added

--- mapp/src/components/pharmaList.jsx
+++ mapp/src/components/pharmaList.jsx
@@ -93,12 +93,28 @@ class PharmaList extends Component{
                     key:unq
                 }
                 a.push(k);
-                this.setState({pharmaList:a});
+                if(this.distance(res.latitude,res.longitude,
+                    this.props.location.state.latitude,this.props.location.state.longitude)<30){
+                        this.setState({pharmaList:a});
+                    }
+                
                 // console.log(this.state.pharmaList);
             })
             .catch(res=>{
                 console.log(`The error is : ${JSON.stringify(res)}`)
             })
+    }
+
+    distance(lat1,lon1,lat2,lon2) {
+        var R = 6371;
+        var dLat = (lat2-lat1) * Math.PI / 180;
+        var dLon = (lon2-lon1) * Math.PI / 180;
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return Math.round(d);
     }
 
 }
