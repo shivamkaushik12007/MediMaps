9bcf837da9ca9baf713e30633418d070810f40b1
kaushik <shivamkaushik12007@gmail.com>
2020-01-24 20:06:05 +0530

    ...

---
+++ mapp-backend/src/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.openUri('mongodb+srv://mapp:<password>@cluster0-xq5iu.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+var db=mongoose.connection;
+db.on('error',function(){
+    console.log("Error connecting to db")
+})
+
+db.once('open',function(){
+    console.log("Connected to db")
+})
